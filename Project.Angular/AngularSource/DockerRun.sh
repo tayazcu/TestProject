a0eec1cd0b7d00ec1c74577710d130b4707a7078
Ali Naghavi <mad.naghavi@gmail.com>
2023-07-26 01:29:09 +0330

    s

---
+++ Project.Angular/AngularSource/DockerRun.sh
@@ -0,0 +1,1 @@
+sudo docker run -p 5002:80 --name testproject-angular -d --restart=always localhost:5000/test/testproject-angular:2023-7-24-v1
---
+++ Project.Angular/AngularSource/docker-entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+envsubst < /usr/share/nginx/html/assets/json/HostConfig.default.json > /usr/share/nginx/html/assets/json/HostConfig.json
+
+exec nginx -g 'daemon off;'
