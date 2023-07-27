fedd4c43d656657c9ef4b09ae558701ffb863720
tayazcu <mad.naghavi@gmail.com>
2023-07-27 08:03:08 +0000

    a

--- Project.Angular/AngularSource/DockerRun.sh
+++ Project.Angular/AngularSource/DockerRun.sh
@@ -1,1 +1,1 @@
-sudo docker run -p 5002:80 --name testproject-angular -d --restart=always localhost:5000/test/testproject-angular:2023-7-24-v1
+sudo docker run -p 5010:80 --name testproject-angular -d --restart=always localhost:5000/test/testproject-angular:2023-7-24-v1
