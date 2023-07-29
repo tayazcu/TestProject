acc63a60f5a28798713b9713aa414f56616517ea
“tayazcu” <mad.naghavi@gmail.com>
2023-07-29 21:06:45 +0000

    aa

--- Project.AngularTest/AngularSrc/DockerRun.sh
+++ Project.AngularTest/AngularSrc/DockerRun.sh
@@ -1,1 +1,1 @@
-sudo docker run -p 4200:80 --name testproject-angular-test -d --restart=always localhost:5000/test/testproject-angular-test:2023-7-24-v1
+sudo docker run -p 4201:80 --name testproject-angular-test -d --restart=always localhost:5000/test/testproject-angular-test:2023-7-24-v1
