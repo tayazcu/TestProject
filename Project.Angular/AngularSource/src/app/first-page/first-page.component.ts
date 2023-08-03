32eb79a778d3f41fde82419a3555f5e6d4564bde
“tayazcu” <mad.naghavi@gmail.com>
2023-08-03 00:28:19 +0000

    rtrtrtrtrtrt

--- Project.Angular/AngularSource/src/app/first-page/first-page.component.ts
+++ Project.Angular/AngularSource/src/app/first-page/first-page.component.ts
@@ -40,7 +40,18 @@ export class FirstPageComponent implements OnInit {
 
   getImages(){
     this.api.get('/api/Picture/Get').subscribe((res:any)=>{
-      this.api2DtaTest = res;
+      debugger
+
+      let finalRes : any[] = []
+
+      res.forEach((element:any) => {
+        let finalRes1;
+        finalRes1 = {...element};
+        finalRes1.dataUrl = "data:" + element.fileType + ';base64,' + element.dbImage;
+        finalRes.push(finalRes1);
+      });
+
+      this.images = finalRes
     });
   }
 
